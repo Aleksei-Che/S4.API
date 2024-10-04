4fbdbde84a33d1801cc4ab4cfb9f5f23ec2e05e1
Aleksei Chesnokov <173167461+Aleksei-Che@users.noreply.github.com>
2024-10-04 14:32:57 +0200

    Ex.4 + Ex.5 / an'adimos API weather y API acudits de Chak Noris

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 "use strict";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -21,17 +20,29 @@ function fetchJoke() {
         return data;
     });
 }
-function updateJoke() {
+// Acudits de Chak Noris
+function fetchChak() {
     return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const jokeData = yield fetchJoke();
-            const jokeElement = document.getElementById("joke");
-            jokeElement.innerHTML = jokeData.joke;
-            console.log(jokeData.joke);
+        const response = yield fetch('https://api.chucknorris.io/jokes/random', {
+            method: "GET",
+        });
+        const data = yield response.json();
+        return data;
+    });
+}
+// Elegir acudit
+function fetchAcudit() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const randomChoice = Math.random() < 0.5;
+        let acudit;
+        if (randomChoice) {
+            acudit = yield fetchJoke();
         }
-        catch (error) {
-            console.log("#Error", error);
+        else {
+            acudit = yield fetchChak();
         }
+        const jokeElement = document.getElementById("joke");
+        jokeElement.innerHTML = acudit.joke ? acudit.joke : acudit.value;
     });
 }
 function vote(score) {
@@ -61,10 +72,34 @@ function setupVoting() {
 }
 function setUpButton() {
     const nextButton = document.getElementById("joke-next");
-    nextButton.addEventListener('click', updateJoke);
+    nextButton.addEventListener('click', fetchAcudit);
+}
+// API Weather
+function fetchWeather() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const weatherElement = document.querySelector('.weather');
+        try {
+            const response = yield fetch('https://api.openweathermap.org/data/2.5/weather?q=Barcelona,es&units=metric&APPID=ae90161fe2a3a0c91d8f8da24e3ff8a2', {
+                method: "GET"
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = yield response.json();
+            console.log(data);
+            const temperature = data.main.temp;
+            const weatherIcon = data.weather[0].icon;
+            const iconURL = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
+            weatherElement.innerHTML = `<img src = "${iconURL}" alt = "Icon weather"> <p> | ${temperature}°С</p>`;
+        }
+        catch (error) {
+            console.log(`Error en obtenir les dades del temps`, error);
+        }
+    });
 }
 window.addEventListener('DOMContentLoaded', () => {
-    updateJoke();
+    fetchAcudit();
     setUpButton();
     setupVoting();
+    fetchWeather();
 });
--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface Joke {
     joke: string,
@@ -26,18 +25,34 @@ async function fetchJoke(): Promise<Joke> {
     
 }
 
-async function updateJoke(): Promise<void> {
+// Acudits de Chak Noris
 
-    try {
-        const jokeData = await fetchJoke();
-        const jokeElement = document.getElementById("joke") as HTMLElement;
-        jokeElement.innerHTML = jokeData.joke;
-        console.log(jokeData.joke)
-    } catch (error){
-        console.log("#Error", error)
-    }
-    
+async function fetchChak(): Promise<any>{
+    const response = await fetch('https://api.chucknorris.io/jokes/random', {
+        method: "GET",
+    });
+    const data = await response.json();
+    return data;
+
 }
+
+// Elegir acudit
+
+async function fetchAcudit(): Promise<void> {
+    const randomChoice = Math.random() < 0.5;
+    let acudit;
+
+    if(randomChoice){
+        acudit = await fetchJoke();
+    } else {
+        acudit = await fetchChak();
+    }
+
+    const jokeElement = document.getElementById("joke") as HTMLElement;
+    jokeElement.innerHTML = acudit.joke ? acudit.joke : acudit.value;
+}
+
+
 
 function vote(score: number): void {
     const jokeElement = document.getElementById("joke") as HTMLElement;
@@ -66,21 +81,46 @@ function setupVoting(): void {
     } else {
         console.error("Error: Voting buttons not founs or incomplete")
     }
-
-    
-
 }
-
 
 
 function setUpButton(): void {
     const nextButton = document.getElementById("joke-next") as HTMLButtonElement;
-    nextButton.addEventListener('click', updateJoke);
+    nextButton.addEventListener('click', fetchAcudit);
 
 }
 
+// API Weather
+
+async function fetchWeather(): Promise<void> {
+    const weatherElement = document.querySelector('.weather') as HTMLElement;
+
+    try{
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Barcelona,es&units=metric&APPID=ae90161fe2a3a0c91d8f8da24e3ff8a2', {
+            method: "GET"
+        });
+
+        if (!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        const temperature = data.main.temp;
+        const weatherIcon = data.weather[0].icon;
+
+        const iconURL = `https://openweathermap.org/img/wn/${weatherIcon}.png`
+
+        weatherElement.innerHTML = `<img src = "${iconURL}" alt = "Icon weather"> <p> | ${temperature}°С</p>`;
+
+
+    }catch(error){
+        console.log(`Error en obtenir les dades del temps`, error)
+    }
+}
+
 window.addEventListener('DOMContentLoaded', ()=>{
-    updateJoke();
+    fetchAcudit()
     setUpButton();
     setupVoting();
+    fetchWeather();
 })
